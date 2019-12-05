a220932dd43a6436e2614563c6bbbfb46b2f3c24
Shohanul Alam <shohan12224943@gmail.com>
2019-12-05 21:59:49 +0600

    Update Client.php
    for calling or using the add  "invoice lines" function the line was needed to be added

--- src/Client.php
+++ src/Client.php
@@ -17,6 +17,7 @@ class Client
         'handles' => 'Handles',
         'hosting' => 'Hosting',
         'invoices' => 'Invoices',
+        'invoiceline' => 'InvoicesLine',
         'orders' => 'Orders',
         'pricequotes' => 'PriceQuotes',
         'products' => 'Products',
